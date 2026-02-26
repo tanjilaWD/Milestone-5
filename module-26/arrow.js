8920f08432c6a6f70571c93de7f146fd7d6936d1
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-02-26 09:50:04 +0600

    basic arrow function

---
+++ module-26/arrow.js
@@ -0,0 +1,18 @@
+const add2 = function(num1, num2){
+    return num1 + num2;
+}
+// console.log(add2(30, 40));
+
+//arrow function
+const add3 = (num1, num2) => num1 + num2;
+console.log(add3(50, 60));
+
+/**
+ * const tenTimes = x => x * 10;
+ * const tenTimes2 = (x) => x * 10;
+ * const getPi = () => 3.1416;
+ * const addAll = (a, b, c, d, e) => a + b + c + d + e;
+ * const isEven = num => num % 2 === 0;
+ */
+
+// document.getElementById('btn').addEventListener('click', () => {})
