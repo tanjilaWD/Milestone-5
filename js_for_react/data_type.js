5314db9b1b5b028061bf81865f8b5cce7dc4215f
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-03-11 15:25:32 +0600

    data type

---
+++ js_for_react/data_type.js
@@ -0,0 +1,10 @@
+/**
+ * two type of Data type in js
+ * 1. Primitive data type(number, string, boolean, undefind, null)
+ * 2. Non primitive or referential data type(arry, object)
+ */
+
+function myFunc(){
+    console.log('Hello');
+}
+console.log(typeof myFunc);
