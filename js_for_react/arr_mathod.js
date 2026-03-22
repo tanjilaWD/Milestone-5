3b1ae5ff3a3fbb673ee2fdfdf7328a77b38f9821
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-03-22 22:05:01 +0600

    data

---
+++ js_for_react/arr_mathod.js
@@ -0,0 +1,106 @@
+const products = [
+  {
+    id: 1,
+    category: "phone",
+    brand: "Apple",
+    model: "iPhone 15",
+    price: 1200,
+    storage: 128,
+    camera: 48,
+    inStock: true
+  },
+  {
+    id: 2,
+    category: "phone",
+    brand: "Samsung",
+    model: "Galaxy S23",
+    price: 950,
+    storage: 256,
+    camera: 50,
+    inStock: true
+  },
+  {
+    id: 3,
+    category: "phone",
+    brand: "Xiaomi",
+    model: "Redmi Note 12",
+    price: 300,
+    storage: 128,
+    camera: 64,
+    inStock: false
+  },
+  {
+    id: 4,
+    category: "phone",
+    brand: "OnePlus",
+    model: "OnePlus 11",
+    price: 700,
+    storage: 256,
+    camera: 50,
+    inStock: true
+  },
+  {
+    id: 5,
+    category: "phone",
+    brand: "Google",
+    model: "Pixel 7",
+    price: 800,
+    storage: 128,
+    camera: 50,
+    inStock: false
+  },
+
+  // 💻 Laptops
+  {
+    id: 6,
+    category: "laptop",
+    brand: "Apple",
+    model: "MacBook Air M2",
+    price: 1400,
+    ram: 8,
+    storage: 256,
+    inStock: true
+  },
+  {
+    id: 7,
+    category: "laptop",
+    brand: "Dell",
+    model: "XPS 13",
+    price: 1200,
+    ram: 16,
+    storage: 512,
+    inStock: true
+  },
+  {
+    id: 8,
+    category: "laptop",
+    brand: "HP",
+    model: "Pavilion 15",
+    price: 700,
+    ram: 8,
+    storage: 512,
+    inStock: false
+  },
+  {
+    id: 9,
+    category: "laptop",
+    brand: "Asus",
+    model: "ROG Zephyrus",
+    price: 1800,
+    ram: 16,
+    storage: 1024,
+    inStock: true
+  },
+  {
+    id: 10,
+    category: "laptop",
+    brand: "Lenovo",
+    model: "ThinkPad X1",
+    price: 1500,
+    ram: 16,
+    storage: 512,
+    inStock: false
+  }
+];
+
+// map method
